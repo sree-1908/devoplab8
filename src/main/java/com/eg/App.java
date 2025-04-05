6aff16f4aed8340fa751bfe97a89fc5d888c1b06
sree-1908 <gayatrisreeja19@gmail.com>
2025-04-05 14:53:10 +0530

    initial commit

---
+++ src/main/java/com/eg/App.java
@@ -0,0 +1,13 @@
+package com.eg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "My name is Sandeep" );
+    }
+}
